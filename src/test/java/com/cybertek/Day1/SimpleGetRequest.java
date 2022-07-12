b5c356327aaf57c0ac8fda65fe73e2a8b7ce8ce6
ecnracr <ecenuracar@hotmail.com>
2022-07-13 00:41:13 +0300

    start

---
+++ src/test/java/com/cybertek/Day1/SimpleGetRequest.java
@@ -0,0 +1,26 @@
+package com.cybertek.Day1;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class SimpleGetRequest {
+
+    String url = "http://34.228.21.3:8000/api/spartans";
+
+    @Test
+    public void test1(){
+//send a get request and save response inside the response object
+        Response response = RestAssured.get(url);
+//print response status code
+        System.out.println(response.statusCode());
+//print response body
+        response.prettyPrint();
+
+
+
+
+
+    }
+
+}
